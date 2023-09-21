9dd60a9a840008e87571c10391f1b3652b5e3095
sergey.yushkevich <sergey.yushkevich@effective-soft.com>
2023-09-21 17:23:49 +0200

    readable names + more variety

--- codegen.js
+++ codegen.js
@@ -1,21 +1,109 @@
+const fs = require('fs')
+function loadSplitLines(name){
+	return fs.readFileSync(name, 'utf-8').split('\n').map(s => s.trim());
+}
+const verbs = loadSplitLines('verbs.txt');
+const adjectives = loadSplitLines('adjectives.txt');
+const nouns = loadSplitLines('nouns.txt');
+function randInt(upperBoundExclusive){
+	return Math.floor(Math.random()*upperBoundExclusive);
+}
+function rand(array){
+	return array[randInt(array.length)];
+}
+function camelCase(parts){
+	return parts.map( (part, index) => {
+		if(index === 0){
+			return part.toLowerCase();
+		}else{
+			return part.substring(0,1).toUpperCase() + part.substring(1).toLowerCase();
+		}
+	}).join('')
+}
+function generateMeaningfulFunctionName(){
+	const parts = [];
+	if(Math.random() < 0.6){
+		parts.push(rand(nouns));
+	}
+	
+	if(parts.length > 0 && Math.random() < 0.3){
+		parts.unshift(rand(adjectives));
+	}
+	
+	if(Math.random() <.95 || parts.length === 0){
+		parts.unshift(rand(verbs));
+	}
+	return camelCase(parts);
+}
+function generateFunctionName(){
+	if(Math.random() < 0.05){
+		return `func${Math.random().toString().substring(2)}`;
+	}else{
+		return generateMeaningfulFunctionName();
+	}
+}
+function generateParamName(){
+	if(Math.random()<0.05){
+		return String.fromCharCode('a'.charCodeAt(0)+randInt(26));
+	}else{
+		const parts = [];
+		if(Math.random()<0.05){
+			parts.push(rand(adjectives));
+		}
+		parts.push(rand(nouns));
+		return camelCase(parts);
+	}
+}
 
-let el = document.querySelector('#header')
+const functionNames = [];
+const functionMaxParamUsed = [];
+function generateFunctionCall(){
+	if(functionNames.length === 0){
+		return 'Math.random()';
+	}
+	const functionIndex = randInt(functionNames.length);
+	const paramsNeeded = functionMaxParamUsed[functionIndex];
+	const params = [];
+	const genericParams = ['result', 'result*2','Math.random()', '(new Date()).getTime()', 'document.querySelectorAll(\'div\').length'];
+	for(let i = 0; i < paramsNeeded; i++){
+		if(Math.random() < 0.3){
+			params.push(rand(genericParams));
+		}else if (Math.random() < 0.05){
+			params.push(generateFunctionCall());
+		}else{
+			params.push(randInt(100));
+		}
+	}
+	return `${functionNames[functionIndex]}( ${params.join(', ')} )`
+}
 
 let res = '';
-const names = [];
+
 function makeFunction(){
-  const name = `func${Math.random().toString().substring(2)}`;
+  const name = generateFunctionName();
   const operations = '+-*/%';
   const op = Math.floor(Math.random()*5);
-  let text =  `function ${name} (...${String.fromCharCode('a'.charCodeAt(0)+Math.floor(Math.random()*26))}) 
+  const varArgsParamName = generateParamName();
+  let text =  `function ${name} (...${varArgsParamName}) 
 {
   result = ${Math.random()*50};
 `;
+  let maxParamsUsed = 0;
   for(let i = 0; i < Math.random()*10; i++){
-    text += `  result = result ${operations[Math.floor(Math.random()*operations.length)]} ${names[Math.floor(Math.random()*names.length)]}();\n`;
+    text += `  result = result ${rand(operations)} ${generateFunctionCall()}`;
+	if(Math.random() < 0.4){
+		const paramIndex = randInt(10);
+		text += ` ${rand(operations)} ${varArgsParamName}[${paramIndex}]`;
+		maxParamsUsed = Math.max(maxParamsUsed, paramIndex+1);
+	}
+	if(Math.random() < 0.05){
+		text += ` ${rand(operations)} ${randInt(100)}`;
+	}
+	text += ';\n'
   }
   text+='  return result;\n}\n\n';
-  names.push(name);
+  functionNames.push(name);
+  functionMaxParamUsed.push(maxParamsUsed);
   return text;
 }
 
@@ -28,4 +116,3 @@ for(let i = 0; i < numFunctions; i++){
 }
 
 console.log(res);
-navigator.clipboard.writeText(res);
